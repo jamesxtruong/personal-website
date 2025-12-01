02e894bd2a7c9e8617f50ba9acf9c36e44cd97e2
jamesxtruong <jamesxtruong@gmail.com>
2025-11-30 22:32:29 -0800

    Add nav bar. Update styles and index.js log.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 // Personal Portfolio - JavaScript
 
 // TODO: Add interactive functionality
+// TODO: Fetch DOM elements using document.querySelector()
+// TODO: Add event listeners (e.g., 'click', 'input', 'submit')
+
+console.log('Portfolio website loaded successfully!');
